54c5728992545a776a9af01698bb64aa1c97293a
Megha03 <megharpatel03@gmail.com>
2024-03-29 19:32:04 +0530

    toggle

--- Envato/src/Layout/Header.jsx
+++ Envato/src/Layout/Header.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.svg'
 import logo2 from '../assets/logo--dark.svg'
+import { IoMdMenu } from "react-icons/io";
 const Header = () => {
     const [colorchange, setColorchange] = useState(false)
-    const changeBg=() =>{
-        if(window.scrollY >= 50){
+    const changeBg = () => {
+        if (window.scrollY >= 50) {
             setColorchange(true)
         }
-        else{
+        else {
             setColorchange(false)
         }
     }
-    window.addEventListener("scroll",changeBg)
-  return ( 
+    window.addEventListener("scroll", changeBg)
+    return (
         <div className={colorchange ? "Navbar_main colorchange" : "Navbar_main"}>
-        <div className="navbar_logo">{colorchange ? <img src={logo2} alt="" /> : <img src={logo} alt="" />}
-              
+            <div className="menu_btn">
+                <IoMdMenu />
+            </div>
+            <div className="navbar_logo">{colorchange ? <img src={logo2} alt="" /> : <img src={logo} alt="" />}
+            </div>
+            <div className="navbar_content">
+                <p>BuyCoin</p>
+                <p>Landing Page</p>
+            </div>
+            <div className="navbar_features">
+                <a href="#home" className='links'>Home</a>
+                <a href="#template" className='links' >Features</a>
+                <a href="#safety" className='links'>Safety</a>
+                <a href="" className='links'>Blog</a>
+                <a href="" className='links'>Contact</a>
+            </div>
+            <div className="nav_signupbutton">
+                <button>Sign In</button>
+            </div>
+
         </div>
-        <div className="navbar_content">
-            <p>BuyCoin</p>
-            <p>Landing Page</p>
-        </div>
-        <div className="navbar_features">
-            <a href="#home" className='links'>Home</a>
-            <a href="#template" className='links' >Features</a>
-            <a href="#safety" className='links'>Safety</a>
-            <a href="" className='links'>Blog</a>
-            <a href="" className='links'>Contact</a>
-        </div>
-        <div className="nav_signupbutton">
-            <button>Sign In</button>
-        </div>
-        </div>
-  )
+    )
 }
 export default Header
--- Envato/src/pages/Faqsec.jsx
+++ Envato/src/pages/Faqsec.jsx
@@ -17,44 +17,45 @@ const Faqsec = () => {
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className={show ?  "plus_icon show" : "plus_icon hide"} onClick={() => setShow(!show)}/>
+            <FaPlus className={show ? "plus_icon show" : "plus_icon"} onClick={() => setShow(!show)} />
           </button>
-          {show && <p >The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>}
+          {show && <p className={show ? "pa show" : "pa"}>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>}
         </div>
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className='plus_icon'  onClick={() => setPara2(!para2)}/>
+            <FaPlus className={para2 ? "plus_icon para2" : "plus_icon"} onClick={() => setPara2(!para2)} />
           </button>
-          {para2 &&<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>}
+          {para2 && <p className={para2 ? "pa para2" : "pa"}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>}
         </div>
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className='plus_icon'  onClick={() => setPara3(!para3)}/>
+            <FaPlus className={para3 ? "plus_icon para3" : "plus_icon"} onClick={() => setPara3(!para3)} />
           </button>
-          {para3 &&<p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always.</p>}
+          {para3 && <p className={para3 ? "pa para3" : "pa"}>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always.</p>}
         </div>
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className='plus_icon'  onClick={() => setPara4(!para4)}/>
+            <FaPlus className={para4 ? "plus_icon para4" : "plus_icon"} onClick={() => setPara4(!para4)} />
           </button>
-          {para4 &&<p>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet</p>}
+          {para4 && <p className={para4 ? "pa para4" : "pa"}>All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet</p>}
         </div>
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className='plus_icon'  onClick={() => setPara5(!para5)}/>
+            <FaPlus className={para5 ? "plus_icon para5" : "plus_icon"}
+              onClick={() => setPara5(!para5)} />
           </button>
-          {para5 &&<p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>}
+          {para5 && <p className={para5 ? "pa para5" : "pa"}>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>}
         </div>
         <div className="faq_buttons">
           <button>
             <span>When will BuyCoin be Listed on exchanges ?</span>
-            <FaPlus className='plus_icon'  onClick={() => setPara6(!para6)}/>
+            <FaPlus className={para6 ? "plus_icon para6" : "plus_icon"} onClick={() => setPara6(!para6)} />
           </button>
-          {para6 &&<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>}
+          {para6 && <p className={para6 ? "pa para6" : "pa"}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>}
         </div>
       </div>
     </div>
