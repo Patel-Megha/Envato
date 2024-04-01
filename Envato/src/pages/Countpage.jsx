5bfb08be79495231d66012dcac98ae3c9b5d7054
Megha03 <megharpatel03@gmail.com>
2024-04-01 19:15:44 +0530

    sdsa

--- Envato/src/pages/Countpage.jsx
+++ Envato/src/pages/Countpage.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import background from '../assets/section-bg3.jpg'
+import CountUp from 'react-countup';
 const Countpage = () => {
   return (
-    <div className='count_section' style={{background: `url(${background})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', backgroundPosition:'center'}}>
-        <div className="gradient_image">
-
+    <div className='count_section' style={{ background: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+      <div className="gradient_image">
+      </div>
+      <div className="text_section">
+        <div className="one">
+          <h1> 
+            <CountUp
+            end={100}
+            duration={15}
+          />
+          </h1>
+          <p>MLN TRANSACTIONS</p>
         </div>
-        <div className="text_section">
-            <div className="one">
-              <h1>5.7</h1>
-              <p>MLN TRANSACTIONS</p>
-            </div>
-            <div className="one">
-              <h1>70</h1>
-              <p>ONLINE CONSULTANTS</p>
-            </div>
-            <div className="one">
-              <h1>23</h1>
-              <p>COUNTRIES SERVED</p>
-            </div>
-            <div className="one">
-              <h1>2.5</h1>
-              <p>MLN BITCOIN WALLETS</p>
-            </div>
+        <div className="one">
+          <h1>
+          <CountUp
+            end={20.5}
+            duration={10}
+          />
+          </h1>
+          <p>ONLINE CONSULTANTS</p>
         </div>
+        <div className="one">
+          <h1>
+          <CountUp
+            end={150}
+            duration={15}
+          />
+          </h1>
+          <p>COUNTRIES SERVED</p>
+        </div>
+        <div className="one">
+          <h1>
+          <CountUp
+            end={50}
+            duration={15}
+          />
+          </h1>
+          <p>MLN BITCOIN WALLETS</p>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default Countpage
--- Envato/src/pages/Ourteam.jsx
+++ Envato/src/pages/Ourteam.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import member6 from '../assets/member6.png'
 import member3 from '../assets/member3.png'
 import member7 from '../assets/member7.png'
@@ -8,36 +8,39 @@ import { FaLinkedin } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 const Ourteam = () => {
+    const [showmedia, setShowmedia] = useState(false)
     return (
         <div className='ourteam_section'>
             <h1>Our Team</h1>
             <p className='ourteam_para'>We are proud of our great team. He is one of the most motivated and is always ready and willing to help out where needed.</p>
+            <div className="hide">
+                <ul>
+                    <li className='instagram'><IoLogoInstagram /></li>
+                    <li className='linkedin'><FaLinkedin /></li>
+                    <li className='facebook'><FaFacebook /></li>
+                    <li className='twitter'><FaTwitter /></li>
+                </ul>
+            </div>
             <div className="team_cards">
                 <div className="member1_card">
                     <p>Roland Samuel</p>
                     <p>CEO</p>
-                    <img src={member6} alt="" height={200}/>
-                    <ul>
-                        <li className='instagram'><IoLogoInstagram /></li>
-                        <li className='linkedin'><FaLinkedin /></li>
-                        <li className='facebook'><FaFacebook /></li>
-                        <li className='twitter'><FaTwitter /></li>
-                    </ul>
+                    <img src={member6} alt="" height={200} />
                 </div>
                 <div className="member1_card">
                     <p>David Drake</p>
                     <p>Head of marketing</p>
-                    <img src={member3} alt="" height={200}/>
+                    <img src={member3} alt="" height={200} />
                 </div>
                 <div className="member1_card">
                     <p>Sandra Pen</p>
                     <p>App developer</p>
-                    <img src={member7} alt="" height={200}/>
+                    <img src={member7} alt="" height={200} />
                 </div>
                 <div className="member1_card">
                     <p>John Smith</p>
                     <p>Manager</p>
-                    <img src={member2} alt="" height={200}/>
+                    <img src={member2} alt="" height={200} />
                 </div>
             </div>
         </div>
